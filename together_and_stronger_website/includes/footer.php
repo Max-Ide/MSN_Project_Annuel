96a8f10c37e09d2ec72d89cbad84e99e5ba7533f
NicoChocoo <nicolascabrera12@gmail.com>
2023-04-13 21:07:42 +0200

    Add folder includes with header.php and footer.php

---
+++ together_and_stronger_website/includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="footer-container">
+        <div class="footer-top">
+            <div class="footer-logo">
+                <h3>Together & Stronger</h3>
+            </div>
+            <nav class="footer-nav">
+                <ul>
+                    <li><a href="index.html">Accueil</a></li>
+                    <li><a href="">Notre histoire</a></li>
+                    <li><a href="#">Activités</a></li>
+                    <li><a href="#">Lieux</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <div class="footer-lang">
+                <ul>
+                    <li><a href="#"><i class="fa fa-flag"></i> FR</a></li>
+                    <li><a href="#"><i class="fa fa-flag"></i> EN</a></li>
+                    <li><a href="#"><i class="fa fa-flag"></i> ES</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Tous droits réservés © 2023 Together & Stronger</p>
+        </div>
+    </div>
+</footer>
---
+++ together_and_stronger_website/includes/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="header-container">
+        <div class="header-content">
+            <h1 class="logo"><a href="index.php" class="logo-link">Together & Stronger</a></h1>
+            <div class="auth-buttons">
+                <button class="login-button" onclick="window.location.href='connection.php';">Se connecter</button>
+                <button class="signup-button" onclick="window.location.href='inscription.php';">S'inscrire</button>
+            </div>
+        </div>
+    </div>
+</header>
