4e2560e9f623b0ef5d6f914bae900df7eac60f08
stephane-tps <71825624+stephane-tps@users.noreply.github.com>
2023-04-21 18:37:02 +0200

    Create index.php

---
+++ together_and_stronger_website/index.php
@@ -0,0 +1,37 @@
+<?php
+// Récupérez l'URL demandée
+$url = $_GET['url'];
+
+// Définissez les routes
+$routes = [
+    '' => 'accueil',
+    'contact' => 'contact',
+    'articles' => 'liste_articles',
+    'article/([0-9]+)' => 'article',
+    'database' => 'database',
+    'entities' => 'entities',
+    'images' => 'images',
+    'includes' => 'includes',
+    'styles' => 'styles',
+    'user' => 'user',
+    'vendor' => 'vendor',
+    'activities.php' => 'activities',
+    'index.php' => 'index',
+    'inscription.php' => 'inscription',
+    'verif_email.php' => 'verif_email'
+];
+
+
+// Parcourez les routes pour trouver celle qui correspond à l'URL
+foreach ($routes as $pattern => $controller) {
+    if ($pattern === $url || preg_match('#^'.$pattern.'$#', $url, $params)) {
+        // Si une route correspond, incluez le contrôleur correspondant
+        include_once 'controllers/'.$controller.'.php';
+        // Appelez la fonction correspondante en passant les paramètres capturés
+        call_user_func_array($controller, array_slice($params, 1));
+        // Sortez de la boucle
+        break;
+    }
+}
+
+
