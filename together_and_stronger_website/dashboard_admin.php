8dc2d1eaa6b661f9490d1e1b081e2557f3c62ad2
NicoChocoo <nicolascabrera12@gmail.com>
2023-05-15 00:07:35 +0200

    Update dashboard_admin.php

--- together_and_stronger_website/dashboard_admin.php
+++ together_and_stronger_website/dashboard_admin.php
@@ -1,12 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
 
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Admin Dashboard</title>
+    <title>Administration</title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="https://use.typekit.net/epq7mib.css">
 </head>
@@ -22,24 +26,24 @@
             <button class="sidebar-admin-btn" id="manage-users-btn">Gestion des utilisateurs</button>
 
             <div class="spacer-admin"></div>
-            <button class="sidebar-admin-btn" id="admin-logout-btn" onclick="window.location.href='index.php';">Se déconnecter</button>
+            <button class="sidebar-admin-btn" id="admin-logout-btn" onclick="window.location.href='deconnection.php';">Se déconnecter</button>
         </div>
         <div class="content-ad">
             <div id="admin-info" class="content-section">
                 <!-- Admin information will be displayed here -->
             </div>
             <div id="manage-activities" class="content-section">
-                <?php include "show_activities.php"; ?>
+            <?php include "show_activities.php"; ?>
                 <button id="add-activity">Add Activity</button>
             </div>
             <div id="manage-clients" class="content-section">
-                <!-- User management will be displayed here -->
+            <?php include "show_companies.php"; ?>
             </div>
             <div id="manage-users" class="content-section">
-                <!-- User management will be displayed here -->
+            <?php include "show_users.php"; ?>
             </div>
         </div>
     </div>
-    <script src="scripts/adminDashboard.js"></script>
+    <script src="js/adminDashboard.js"></script>
 </body>
 </html>
